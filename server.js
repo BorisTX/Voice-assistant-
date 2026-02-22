392a9c9ee5c1fbf99f454d9117a2712851d44bdb
BorisTX <heansal35@mail.ru>
2026-02-22 15:01:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ wss.on("connection", (twilioWs) => {
     "wss://api.openai.com/v1/realtime?model=gpt-realtime",
     {
       headers: {
-        Authorization: Bearer ${process.env.OPENAI_API_KEY},
+        Authorization: 'Bearer ${process.env.OPENAI_API_KEY}',
       },
     }
   );
