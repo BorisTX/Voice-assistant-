0e3507df0a4a773b60e59a8ba93dada1a2c16cd5
BorisTX <heansal35@mail.ru>
2026-02-22 20:48:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ import WebSocket, { WebSocketServer } from "ws";
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-
+app.use((req, res, next) => {
+  console.log("INCOMING:", req.method, req.url);
+  next();
+});
 // Health check
 app.get("/", (req, res) => res.status(200).send("OK"));
 
