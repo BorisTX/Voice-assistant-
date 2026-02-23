68642747a1af2dfe3eadb03b4a7f64d023191d7f
BorisTX <heansal35@mail.ru>
2026-02-22 21:16:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import WebSocket, { WebSocketServer } from "ws";
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 
-});
+
 app.use((req, res, next) => {
   console.log("INCOMING:", req.method, req.url);
   next();
