3aa0f73174b16baada36fd3e59b88638d33e77ca
BorisTX <heansal35@mail.ru>
2026-02-23 20:53:41 -0600

    Update googleAuth.js

--- googleAuth.js
+++ googleAuth.js
@@ -11,7 +11,7 @@ export function makeOAuthClient() {
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
   const redirectUri = process.env.GOOGLE_REDIRECT_URI;
 
-  if (!clientId  !clientSecret  !redirectUri) {
+  if (!clientId || !clientSecret || !redirectUri) {
     throw new Error("Missing GOOGLE_CLIENT_ID / GOOGLE_CLIENT_SECRET / GOOGLE_REDIRECT_URI");
   }
 
