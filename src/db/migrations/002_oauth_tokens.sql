0f341ea33a64edfbc8e315efaf4adf79edba4f66
BorisTX <heansal35@mail.ru>
2026-02-26 10:57:56 -0600

    Update 002_oauth_tokens.sql

--- src/db/migrations/002_oauth_tokens.sql
+++ src/db/migrations/002_oauth_tokens.sql
@@ -1,12 +1,1 @@
-CREATE TABLE IF NOT EXISTS oauth_tokens (
-  id INTEGER PRIMARY KEY,
-  access_token TEXT,
-  refresh_token TEXT,
-  scope TEXT,
-  token_type TEXT,
-  expiry_date INTEGER,
-  updated_at INTEGER
-);
 
-INSERT OR IGNORE INTO oauth_tokens (id, updated_at)
-VALUES (1, strftime('%s','now'));
